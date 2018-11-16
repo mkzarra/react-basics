1352640d2470afd86b9ddc17bb1660278feee2b2
mkzarra <mkzarra@gmail.com>
2018-11-16 10:01:34 -0500

    added Radium and styles using Radium

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Person from './Person/Person';
 import './App.css';
+import Radium, { StyleRoot } from 'radium';
 
 class App extends Component {
   state = {
@@ -45,7 +46,11 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     }
 
     let persons = null
@@ -65,19 +70,33 @@ class App extends Component {
           </div>
       )
       style.backgroundColor = 'red'
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
+    }
+
+    const classes = []
+    if (this.state.persons.length <= 2) {
+      classes.push('red')
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push('bold')
     }
 
     return (
+      <StyleRoot>
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
+        <p className={classes.join(' ')} >This is really working!</p>
         <button
           style={style}
           onClick={this.togglePersonHandler}>Toggle Persons</button>
         {persons}
-      </div>
+        </div>
+        </StyleRoot>
     );
   }
 }
 
-export default App;
+export default Radium(App);
